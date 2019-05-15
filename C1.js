472667dc2089397063000a4979dbdec5608eefee
memor81 <50591628+memor81@users.noreply.github.com>
2019-05-15 12:20:05 -0300

    Add files via upload

---
+++ C1.js
@@ -0,0 +1,313 @@
+﻿
+var Comercio = {
+          "type": "FeatureCollection",
+          "features": [
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65041564, -32.93935128] },
+                "type": "Feature", "properties": { "popupContent": "Tucumán 2066 " }, "id": 181},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63947026, -32.94391098] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 1221  " }, "id": 543},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6478618, -32.9499392] },
+                "type": "Feature", "properties": { "popupContent": "España 1190 " }, "id": 573},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64706404, -32.94676528] },
+                "type": "Feature", "properties": { "popupContent": "España 944 " }, "id": 700},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.62971374, -32.95412855] },
+                "type": "Feature", "properties": { "popupContent": "Alem Leandro N. 1271 " }, "id": 750},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63645148, -32.94941604] },
+                "type": "Feature", "properties": { "popupContent": "San Luis 932 " }, "id": 1744},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63485108, -32.94710176] },
+                "type": "Feature", "properties": { "popupContent": "Córdoba 864 " }, "id": 1813},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64585913, -32.93707441] },
+                "type": "Feature", "properties": { "popupContent": "Italia 143 " }, "id": 1845},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63703248, -32.95187804] },
+                "type": "Feature", "properties": { "popupContent": "Mendoza 928 " }, "id": 2668},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64472703, -32.94404212] },
+                "type": "Feature", "properties": { "popupContent": "Presidente Roca 719 " }, "id": 3076},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6462931, -32.93649974] },
+                "type": "Feature", "properties": { "popupContent": "Jujuy 1827 " }, "id": 3093},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63387453, -32.95243643] },
+                "type": "Feature", "properties": { "popupContent": "Buenos Aires 1193 " }, "id": 4536},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6436274, -32.9439969] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 1510 " }, "id": 4542},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6422308, -32.94325108] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 1426 " }, "id": 4862},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6505519, -32.94935242] },
+                "type": "Feature", "properties": { "popupContent": "Dorrego 1182 " }, "id": 5038},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64862251, -32.94811656] },
+                "type": "Feature", "properties": { "popupContent": "Italia 1069 " }, "id": 5216},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65309059, -32.95277664] },
+                "type": "Feature", "properties": { "popupContent": "Zeballos 2019 " }, "id": 5301},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.648642, -32.94177948] },
+                "type": "Feature", "properties": { "popupContent": "Dorrego 564 " }, "id": 5478},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64184276, -32.95495546] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1514 " }, "id": 5687},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6489695, -32.93591904] },
+                "type": "Feature", "properties": { "popupContent": "Jujuy 2026 " }, "id": 6036},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64652876, -32.9550429] },
+                "type": "Feature", "properties": { "popupContent": "Montevideo 1518 " }, "id": 6124},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64193324, -32.95611792] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1617 " }, "id": 6176},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6264765, -32.95757039] },
+                "type": "Feature", "properties": { "popupContent": "Zeballos 101 " }, "id": 6211},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.62798136, -32.95729036] },
+                "type": "Feature", "properties": { "popupContent": "Zeballos 212 " }, "id": 6213},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63794534, -32.94172208] },
+                "type": "Feature", "properties": { "popupContent": "Tucumán 1159 " }, "id": 6264},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63722489, -32.94444982] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 1063 " }, "id": 6396},
+
+
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64498476, -32.94277348] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 1624 " }, "id": 6419},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6468175, -32.9425482] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 1757 " }, "id": 6421},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64753746, -32.93986632] },
+                "type": "Feature", "properties": { "popupContent": "Tucumán 1854 " }, "id": 6438},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65138604, -32.93927757] },
+                "type": "Feature", "properties": { "popupContent": "Tucumán 2141 " }, "id": 6451},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63568748, -32.945857] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 942 " }, "id": 6458},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63840444, -32.94496844] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 1132 " }, "id": 6462},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63886072, -32.94486372] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 1166 " }, "id": 6465},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64687232, -32.94340596] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 1744 " }, "id": 6486},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63234494, -32.95399752] },
+                "type": "Feature", "properties": { "popupContent": "Tres De Febrero 562 " }, "id": 6819},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6316221, -32.9529422] },
+                "type": "Feature", "properties": { "popupContent": "Mendoza 535 " }, "id": 6984},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63473588, -32.952304] },
+                "type": "Feature", "properties": { "popupContent": "Mendoza 748 " }, "id": 6997},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63506754, -32.95101825] },
+                "type": "Feature", "properties": { "popupContent": "San Juan 801 " }, "id": 7042},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63769282, -32.94919436] },
+                "type": "Feature", "properties": { "popupContent": "San Luis 1034 " }, "id": 7090},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64112212, -32.94780162] },
+                "type": "Feature", "properties": { "popupContent": "Ricardone 1253 " }, "id": 7096},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64125076, -32.94777826] },
+                "type": "Feature", "properties": { "popupContent": "Ricardone 1269 " }, "id": 7097},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64920896, -32.93276678] },
+                "type": "Feature", "properties": { "popupContent": "Balcarce 166 Bis" }, "id": 7296},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6502214, -32.936789] },
+                "type": "Feature", "properties": { "popupContent": "Balcarce 180 " }, "id": 7302},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6506306, -32.938416] },
+                "type": "Feature", "properties": { "popupContent": "Balcarce 340 " }, "id": 7307},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6508819, -32.9393923] },
+                "type": "Feature", "properties": { "popupContent": "Balcarce 402 " }, "id": 7308},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64835766, -32.93586507] },
+                "type": "Feature", "properties": { "popupContent": "Moreno 81 " }, "id": 7345},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6498454, -32.9408965] },
+                "type": "Feature", "properties": { "popupContent": "Moreno 510 " }, "id": 7356},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65008156, -32.94748014] },
+                "type": "Feature", "properties": { "popupContent": "Dorrego 1034 " }, "id": 7388},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64992416, -32.94764829] },
+                "type": "Feature", "properties": { "popupContent": "Dorrego 1051 " }, "id": 7390},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65107112, -32.95139352] },
+                "type": "Feature", "properties": { "popupContent": "Dorrego 1362 " }, "id": 7398},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64863925, -32.948184] },
+                "type": "Feature", "properties": { "popupContent": "Italia 1075 " }, "id": 7446},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65034916, -32.95417672] },
+                "type": "Feature", "properties": { "popupContent": "Italia 1576 " }, "id": 7456},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65032391, -32.95487008] },
+                "type": "Feature", "properties": { "popupContent": "Italia 1639 " }, "id": 7457},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64509636, -32.93895294] },
+                "type": "Feature", "properties": { "popupContent": "España 282 " }, "id": 7464},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64756984, -32.95457496] },
+                "type": "Feature", "properties": { "popupContent": "Presidente Roca 1568 " }, "id": 7637},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64441383, -32.94861075] },
+                "type": "Feature", "properties": { "popupContent": "Paraguay 1047 " }, "id": 7690},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64068036, -32.93867212] },
+                "type": "Feature", "properties": { "popupContent": "Corrientes 194 " }, "id": 7718},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6415254, -32.9420054] },
+                "type": "Feature", "properties": { "popupContent": "Corrientes 480 " }, "id": 7725},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63929244, -32.9397231] },
+                "type": "Feature", "properties": { "popupContent": "Entre Ríos 261 " }, "id": 7836},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64105665, -32.94675562] },
+                "type": "Feature", "properties": { "popupContent": "Entre Ríos 867 " }, "id": 7859},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63806665, -32.94072844] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 343 " }, "id": 7901},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6378218, -32.9410818] },
+                "type": "Feature", "properties": { "popupContent": "Pasaje Simeoni 1160 " }, "id": 7906},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63961653, -32.94685566] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 853 " }, "id": 7929},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64024244, -32.9485602] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 968 " }, "id": 7937},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64011729, -32.9488464] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1029 " }, "id": 7938},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64103604, -32.95175776] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1236 " }, "id": 7949},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64109001, -32.95276845] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1319 " }, "id": 7956},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64107929, -32.95315605] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1351 " }, "id": 7957},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63886383, -32.94955518] },
+                "type": "Feature", "properties": { "popupContent": "Sarmiento 1043 " }, "id": 7998},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6403893, -32.9549265] },
+                "type": "Feature", "properties": { "popupContent": "Sarmiento 1490 " }, "id": 8007},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63716013, -32.94911739] },
+                "type": "Feature", "properties": { "popupContent": "San Martín 983 " }, "id": 8035},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6388621, -32.95587117] },
+                "type": "Feature", "properties": { "popupContent": "San Martín 1549 " }, "id": 8052},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63415947, -32.94861219] },
+                "type": "Feature", "properties": { "popupContent": "Laprida 901 " }, "id": 8179},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63431909, -32.94917293] },
+                "type": "Feature", "properties": { "popupContent": "Laprida 947 " }, "id": 8181},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63449598, -32.95493001] },
+                "type": "Feature", "properties": { "popupContent": "Buenos Aires 1401 " }, "id": 8211},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63453748, -32.95429322] },
+                "type": "Feature", "properties": { "popupContent": "Buenos Aires 1346 " }, "id": 8212},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.6295785, -32.95357425] },
+                "type": "Feature", "properties": { "popupContent": "Alem Leandro N. 1225 " }, "id": 8251},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.62994159, -32.95506159] },
+                "type": "Feature", "properties": { "popupContent": "Alem Leandro N. 1349 " }, "id": 8254},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63024075, -32.95027875] },
+                "type": "Feature", "properties": { "popupContent": "Primero De Mayo 975 " }, "id": 8271},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63087305, -32.9529757] },
+                "type": "Feature", "properties": { "popupContent": "Primero De Mayo 1195 " }, "id": 8275},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.62723148, -32.95484312] },
+                "type": "Feature", "properties": { "popupContent": "Colón Cristóbal 1284 " }, "id": 8298},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.62758992, -32.95641182] },
+                "type": "Feature", "properties": { "popupContent": "Colón Cristóbal 1422 " }, "id": 8300},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65174896, -32.9367431] },
+                "type": "Feature", "properties": { "popupContent": "Oroño 198 " }, "id": 8887},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63041033, -32.95315213] },
+                "type": "Feature", "properties": { "popupContent": "Mendoza 451 " }, "id": 10408},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63259086, -32.95395688] },
+                "type": "Feature", "properties": { "popupContent": "Tres De Febrero 578 " }, "id": 10575},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65068204, -32.94176136] },
+                "type": "Feature", "properties": { "popupContent": "San Lorenzo 2042 " }, "id": 10694},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64634645, -32.9390468] },
+                "type": "Feature", "properties": { "popupContent": "Italia 311 " }, "id": 10838},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63485544, -32.95722639] },
+                "type": "Feature", "properties": { "popupContent": "Montevideo 673 " }, "id": 11389},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.65064324, -32.93924195] },
+                "type": "Feature", "properties": { "popupContent": "Balcarce 379 " }, "id": 11661},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64444986, -32.94394291] },
+                "type": "Feature", "properties": { "popupContent": "Santa Fe 1569 " }, "id": 11722},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64499056, -32.95582136] },
+                "type": "Feature", "properties": { "popupContent": "Corrientes 1636 " }, "id": 11808},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64081406, -32.95166914] },
+                "type": "Feature", "properties": { "popupContent": "Mitre 1229 " }, "id": 11815},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64970784, -32.95163116] },
+                "type": "Feature", "properties": { "popupContent": "Italia 1352 " }, "id": 12040},
+
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.63948946, -32.93771534] },
+                "type": "Feature", "properties": { "popupContent": "Jujuy 1341 " }, "id": 12442},
+
+       { "geometry": { "type": "Point", "coordinates": [ -60.64065606, -32.94110614] },
+                "type": "Feature", "properties": { "popupContent": "Tucumán 1354 " }, "id": 13642} ] };
+
