0060826dbcaba74119d24fcfab2acd503ad2e551
memor81 <50591628+memor81@users.noreply.github.com>
2019-05-15 12:01:43 -0300

    Add files via upload

---
+++ js/leaflet-color-markers.js
@@ -0,0 +1,92 @@
+var blueIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-blue.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-red.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-green.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-orange.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-yellow.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-violet.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-grey.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: 'images/marker-icon-2x-black.png',
+	shadowUrl: 'images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var nIcon = L.icon({
+    iconUrl: 'images/n.png',
+	iconSize: [8, 8],
+    iconAnchor:   [4, 8],
+    popupAnchor:  [1, -8]
+});
+
+var cIcon = L.icon({
+    iconUrl: 'images/c.png',
+	iconSize: [9, 9],
+    iconAnchor:   [4, 9],
+    popupAnchor:  [1, -9]
+});
+
+var vIcon = L.icon({
+    iconUrl: 'images/v.png',
+	iconSize: [8, 8],
+    iconAnchor:   [4, 8],
+    popupAnchor:  [1, -8]
+});
